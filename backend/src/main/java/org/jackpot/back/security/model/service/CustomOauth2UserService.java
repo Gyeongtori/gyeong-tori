f611d4b5419262c26d3a5e559ec683d528074aad
MunHyerin <9993hye@naver.com>
2024-01-24 12:11:03 +0900

    chore: SecurityConfig OAuth2 추가 S10P12C107-56

---
+++ backend/src/main/java/org/jackpot/back/security/model/service/CustomOauth2UserService.java
@@ -0,0 +1,15 @@
+package org.jackpot.back.security.model.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOauth2UserService extends DefaultOAuth2UserService {
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) {
+        return null;
+    }
+}
