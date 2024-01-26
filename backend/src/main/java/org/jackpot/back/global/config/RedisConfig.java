ce053c9c2b7748b79b98896691989a5da17488cf
김다희 <dhmonukim24@gmail.com>
2024-01-26 17:50:33 +0900

    feat: socket.io 설정 추가

--- backend/src/main/java/org/jackpot/back/global/config/RedisConfig.java
+++ backend/src/main/java/org/jackpot/back/global/config/RedisConfig.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -20,14 +22,14 @@ public class RedisConfig {
 
     // lettuce
     @Bean
-//    @Profile("dev")
+    @Profile("dev")
     public RedisConnectionFactory redisConnectionFactoryDev() {
         return new LettuceConnectionFactory(
                 new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
     }
 
     @Bean
-//    @Profile("dev")
+    @Profile("dev")
     public RedisTemplate<?, ?> redisTemplateDev() {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactoryDev());   //connection
@@ -36,24 +38,24 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-//    @Bean
-//    @Profile("prod")
-//    public RedisConnectionFactory redisConnectionFactoryProd() {
-//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-//        config.setHostName(redisProperties.getHost());
-//        config.setPort(redisProperties.getPort());
-//        config.setPassword(RedisPassword.of(redisProperties.getPassword()));
-//        return new LettuceConnectionFactory(config);
-//    }
-//
-//    // Redis template
-//    @Bean
-//    @Profile("prod")
-//    public RedisTemplate<?, ?> redisTemplateProd() {
-//        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redisConnectionFactoryProd());   //connection
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());    // key
-//        redisTemplate.setValueSerializer(new StringRedisSerializer());  // value
-//        return redisTemplate;
-//    }
+    @Bean
+    @Profile("prod")
+    public RedisConnectionFactory redisConnectionFactoryProd() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.getHost());
+        config.setPort(redisProperties.getPort());
+        config.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        return new LettuceConnectionFactory(config);
+    }
+
+    // Redis template
+    @Bean
+    @Profile("prod")
+    public RedisTemplate<?, ?> redisTemplateProd() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactoryProd());   //connection
+        redisTemplate.setKeySerializer(new StringRedisSerializer());    // key
+        redisTemplate.setValueSerializer(new StringRedisSerializer());  // value
+        return redisTemplate;
+    }
 }
---
+++ backend/src/main/java/org/jackpot/back/socket/model/Location.java
@@ -0,0 +1,15 @@
+package org.jackpot.back.socket.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+    private String lng;
+    private String lat;
+    private String username;
+    private Long userId;
+}
