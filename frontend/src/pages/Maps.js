5d905bdca1e250cb1b757da9903c76f7e2134c73
Onyu Kim <gkc04075@gmail.com>
2024-01-31 08:00:20 +0900

    chore: 지도에 기능 추가중

--- frontend/src/pages/Maps.js
+++ frontend/src/pages/Maps.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo, useState, useCallback } from "react";
 import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import styled from "styled-components";
+import InfoTop from "../components/Mains/InfoTop";
 
 // 위도 경도 sample
 // 서울 : 37.566535   126.8154368
@@ -8,18 +10,47 @@ import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 
 
 export default function Maps () {
+  
+  // const center = {lat: 35.175595 , lng: 126.907032 }
+
+  const [map, setMap] = useState(null);
+	const [center, setCenter] = useState({ lat: 35.175595, lng: 126.907032 });
+	const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
+
+
   const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
     googleMapsApiKey: "AIzaSyAfcce2IjhzDkYHn7rZBilMDHw4f1c4IwU"
   });
 
   if ( !isLoaded ) return <div>Loading...</div>
 
-  const getNow = () => {
+  const locateUser = useCallback(() => {
+		if (center.lat && center.lng) {
+			const newLocation = {
+				lat: center.lat,
+				lng: center.lng,
+			};
+			setMapCenter(newLocation);
+			if (map) {
+				map.panTo(newLocation);
+				map.setZoom(18);
+			}
+		}
+	}, [center.lat, center.lng, map]);
+
+	const onUnmount = useCallback(function callback() {
+		setMap(null);
+	}, []);
+  
+
+  const getLocation = () => {
     if (navigator.geolocation) { // GPS를 지원하면
       navigator.geolocation.getCurrentPosition(function(position) {
         const latNow = position.coords.latitude
         const lngNow = position.coords.longitude
         console.log(latNow, lngNow)
+        // setCurrentPosition({x: latNow, y: lngNow})
   
         // alert(position.coords.latitude + ' ' + position.coords.longitude);
       }, function(error) {
@@ -35,30 +66,6 @@ export default function Maps () {
   }
 
 
-  function getLocation() {
-    if (navigator.geolocation) { // GPS를 지원하면
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const latNow = position.coords.latitude
-        const lngNow = position.coords.longitude
-        console.log(latNow, lngNow)
-  
-        // alert(position.coords.latitude + ' ' + position.coords.longitude);
-      }, function(error) {
-        console.error(error);
-      }, {
-        enableHighAccuracy: false,
-        maximumAge: 0,
-        timeout: Infinity
-      });
-    } else {
-      alert('GPS를 지원하지 않습니다');
-    }
-  }
-  
-
-
-  const center = {lat: 35.175595 , lng: 126.907032 }
-  
 
   // useEffect(() => {
   //   getLocation();
@@ -70,8 +77,6 @@ export default function Maps () {
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <div style={{ position: 'relative' }}>
-        <div style={{zIndex: 10, backgroundColor: '#FFAAAA'}}>testtttttttt</div>
-  
         <GoogleMap 
           zoom={18} 
           center={center} 
@@ -80,23 +85,30 @@ export default function Maps () {
           // 기본 ui 요소 지우기
           options={{disableDefaultUI: true}}
 
-          mapContainerStyle={{ width: '100%', height: '100vh', position: 'absolute' }}
+          mapContainerStyle={{ width: '100%', height: '100vh' }}
         > 
           <MarkerF position={center} />
-          <div style={{ position: 'absolute', backgroundColor: '#FFAAAA', zIndex: 10}}>Testtesttest</div>
-          <button onClick={() => getNow()} style={{ position: 'absolute', zIndex: 10 }}>버튼</button>
-          {/* <div>제발나와라ㅏㅏ아아아ㅏ아앙</div>
-          <div>제발나와라ㅏㅏ아아아ㅏ아앙</div>
-          <div>제발나와라ㅏㅏ아아아ㅏ아앙</div>
-          <div>제발나와라ㅏㅏ아아아ㅏ아앙</div> */}
+          <Test>
+            <InfoTop ></InfoTop>
+            <button onClick={() => getLocation()} >버튼</button>
+          </Test>
           
-          <div style={{width: '100%', height: '100%', position: 'absolute', zIndex: 10}}>제발나와라ㅏㅏ아아아ㅏ아앙</div>
+          
+  
         </GoogleMap>
 
       </div>
     </div>
   )
 }
+
+const Test = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 10;
+
+`;
 
 
 
