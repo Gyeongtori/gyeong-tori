2c19df37c9bd6b8b1f62fbe3e8120a5c9657ea90
김온유 <gkc04075@gmail.com>
2024-01-31 17:51:45 +0900

    chore: 지도 내위치 옵션 추가

--- frontend/src/pages/Maps.js
+++ frontend/src/pages/Maps.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState, useCallback } from "react";
-import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import { GoogleMap, Circle, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 import styled from "styled-components";
 import InfoTop from "../components/Mains/InfoTop";
 
@@ -8,12 +8,14 @@ import InfoTop from "../components/Mains/InfoTop";
 // 싸피 : 35.205216   126.811744
 // 전대 : 35.175595   126.907032
 
+const google = window.google = window.google ? window.google : {}
 
 export default function Maps () {
   
   // const center = {lat: 35.175595 , lng: 126.907032 }
 
   const [map, setMap] = useState(null);
+  // 전대 기준
 	const [center, setCenter] = useState({ lat: 35.175595, lng: 126.907032 });
 	const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
 
@@ -23,15 +25,16 @@ export default function Maps () {
     googleMapsApiKey: "AIzaSyAfcce2IjhzDkYHn7rZBilMDHw4f1c4IwU"
   });
 
-  if ( !isLoaded ) return <div>Loading...</div>
-
+  
+// ???
   const locateUser = useCallback(() => {
 		if (center.lat && center.lng) {
 			const newLocation = {
-				lat: center.lat,
+        lat: center.lat,
 				lng: center.lng,
 			};
-			setMapCenter(newLocation);
+      console.log('newLocation: ', newLocation);
+			setCenter(newLocation);
 			if (map) {
 				map.panTo(newLocation);
 				map.setZoom(18);
@@ -39,20 +42,22 @@ export default function Maps () {
 		}
 	}, [center.lat, center.lng, map]);
 
-	const onUnmount = useCallback(function callback() {
+  const onUnmount = useCallback(function callback() {
+		// 컴포넌트가 언마운트될때 호출 map 상태 변수를 null로 설정하여 초기화
 		setMap(null);
 	}, []);
-  
 
+
+
+// 내위치 업데이트
   const getLocation = () => {
     if (navigator.geolocation) { // GPS를 지원하면
       navigator.geolocation.getCurrentPosition(function(position) {
         const latNow = position.coords.latitude
         const lngNow = position.coords.longitude
         console.log(latNow, lngNow)
-        // setCurrentPosition({x: latNow, y: lngNow})
-  
-        // alert(position.coords.latitude + ' ' + position.coords.longitude);
+        setCenter({lat: latNow, lng: lngNow});
+
       }, function(error) {
         console.error(error);
       }, {
@@ -71,40 +76,67 @@ export default function Maps () {
   //   getLocation();
   // }, [])
   
-  getLocation();
+  // getLocation();
 
+  // 내 위치 표시 range
+  const circleRangeOptions = {
+		strokeColor: '#FF7575',
+		strokeOpacity: 0,
+		strokeWeight: 0,
+		fillColor: '#C779D0',
+		fillOpacity: 0.35,
+		radius: 100,
+		center,
+	};
+
+  // 내 특정 위치 표시
+  const markerCircleOptions = {
+		strokeColor: '#FFFFFF',
+		strokeOpacity: 1,
+		strokeWeight: 2,
+		fillColor: '#C779D0',
+		fillOpacity: 1,
+		radius: 10,
+		center,
+
+	};
+
+
+  if ( !isLoaded ) return <div>Loading...</div>
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <div style={{ position: 'relative' }}>
         <GoogleMap 
-          zoom={18} 
+          zoom={17} 
           center={center} 
           mapContainerClassName="map-container"
-          
+          onUnmount={onUnmount}
           // 기본 ui 요소 지우기
-          options={{disableDefaultUI: true}}
+          options={{disableDefaultUI: true, minZoom: 16, maxZoom: 18,}}
 
           mapContainerStyle={{ width: '100%', height: '100vh' }}
         > 
-          <MarkerF position={center} />
-          <Test>
-            <InfoTop ></InfoTop>
+          {/* <MarkerF position={center} /> */}
+          <Circle center={center} options={circleRangeOptions} />
+          <Circle center={center} options={markerCircleOptions} />
+          <Body>
             <button onClick={() => getLocation()} >버튼</button>
-          </Test>
-          
+            <InfoTop ></InfoTop>
+          </Body>
           
   
         </GoogleMap>
 
+          
       </div>
     </div>
   )
 }
 
-const Test = styled.div`
+const Body = styled.div`
   width: 100%;
-  height: 100%;
+  /* height: 100%; */
   position: absolute;
   z-index: 10;
 
