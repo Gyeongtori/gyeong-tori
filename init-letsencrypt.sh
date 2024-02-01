b81ed532457e77b571a47cb5dc65557ab0d9a460
김다희 <dhmonukim24@gmail.com>
2024-02-01 15:44:39 +0900

    chore: volume 테스트 S10P12C107-25

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,9 +7,11 @@ fi
 
 domains="i10c107.p.ssafy.io"
 rsa_key_size=4096
-data_path="../certbot"
+data_path="./certbot"
 email="dhmonukim24@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+ls -al
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
