a13cec1ee2c78a78e065d93fa5a58f12c04bf67d
kimdahui42 <dhmonukim24@gmail.com>
2024-02-02 01:12:16 +0900

    chore: 사용 없는 코드 정리, 도커 볼륨 매핑 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -10,13 +10,13 @@ rsa_key_size=4096
 data_path="./certbot"
 email="dhmonukim24@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
+#
+#if [ -d "$data_path" ]; then
+#  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+#  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+#    exit
+#  fi
+#fi
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
