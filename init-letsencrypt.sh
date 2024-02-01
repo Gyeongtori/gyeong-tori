6e0bfc13a683a1f7a00fe6ef2568fbcf34e95ddb
kimdahui42 <dhmonukim24@gmail.com>
2024-02-01 22:20:12 +0900

    chore: 폴더 접근 경로 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 set -x
-if ! [ -x "$(command -v docker compose)" ]; then
-  echo 'Error: docker compose is not installed.' >&2
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed.' >&2
   exit 1
 fi
 
-domains="p.ssafy.io"
+domains=("p.ssafy.io") # Make domains an array if multiple domains are needed
 rsa_key_size=4096
 data_path="./certbot"
 email="dhmonukim24@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-
-#if [ -d "$data_path" ]; then
-#  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-#  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-#    exit
-#  fi
-#fi
-
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
@@ -27,65 +19,39 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### copy files to nginx ..."
-if [ ! -e "/etc/letsencrypt/options-ssl-nginx.conf" ]
-  cp $data_path/conf /etc/letsencrypt
-  cp $data_path/www /var/www/certbot
+echo "### Copying files to nginx ..."
+if [ ! -e "/etc/letsencrypt/options-ssl-nginx.conf" ]; then
+  cp -r $data_path/conf/* /etc/letsencrypt
+  cp -r $data_path/www/* /var/www/certbot
 fi
 echo
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker compose -p test-server run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
+echo "### Creating dummy certificate for ${domains[@]} ..."
+for domain in "${domains[@]}"; do
+  path="/etc/letsencrypt/live/$domain"
+  mkdir -p "$path"
+  docker-compose -p test-server run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+      -keyout '$path/privkey.pem' \
+      -out '$path/fullchain.pem' \
+      -subj '/CN=localhost'" certbot
+done
 echo
-
 
 echo "### Starting nginx ..."
-docker compose -p test-server up -d nginx || docker compose -p test-server restart nginx
+docker-compose -p test-server up -d nginx || docker-compose -p test-server restart nginx
 echo
 
-
-
-echo "### Deleting dummy certificate for $domains ..."
-docker compose -p test-server run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo "### Deleting dummy certificate for ${domains[@]} ..."
+for domain in "${domains[@]}"; do
+  docker-compose -p test-server run --rm --entrypoint "\
+    rm -Rf /etc/letsencrypt/live/$domain && \
+    rm -Rf /etc/letsencrypt/archive/$domain && \
+    rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+done
 echo
 
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
+echo "### Requesting Let's Encrypt certificate for ${domains[@]} ..."
 domain_args=""
 for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
-
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
-
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
-
-docker compose -p test-server run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-echo
-
-docker logs nginx
-
-echo "### Reloading nginx ..."
-docker compose -p test-server exec nginx nginx -s reload
+  domain_args="$domain_args -d $domain
