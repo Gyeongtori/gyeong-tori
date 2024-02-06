1e643af5e8b4835b4fa027478646ec6d5c8093a9
탁하윤 <thagyun@naver.com>
2024-02-06 11:35:27 +0900

    feat: 배틀 소켓 연결

--- frontend/src/components/Battle/Radar.js
+++ frontend/src/components/Battle/Radar.js
@@ -13,7 +13,10 @@ import io from "socket.io-client";
 
 // 1. io.connect와 동일하게 설정
 // 2. 백엔드 서버에 대한 URL 전달
-const socket = io.connect("http://localhost:8085");
+const socket = io.connect(`${process.env.REACT_APP_SOCKET_SERVER_ADDRESS}`, {
+  cors: { origin: "*" },
+  transports: ["websocket"],
+});
 
 const OuterCircle = styled.div`
   width: 240px;
@@ -47,6 +50,7 @@ const OuterCircle = styled.div`
       transform: scale(2.5);
     }
   }
+  z-index: -1;
 `;
 
 const GreenScanner = styled.div`
@@ -64,8 +68,27 @@ const GreenScanner = styled.div`
       transform: rotate(1turn);
     }
   }
+  z-index: -1;
 `;
 const Radar = () => {
+  // message를 받을 때마다 호출
+  // 즉, 서버에서 이벤트가 발생할 때마다 실행되는 함수와 같은 역할
+
+  useEffect(() => {
+    // 이벤트가 발생할 경우를 대비하여 이벤트를 수신하고 있다고 선언
+    // socket.on() : 이벤트 수신
+    // .on("수신 이벤트 함수", 데이터 수신 콜백 함수)
+    // socket.on("receive_message", (data) => {
+    //   // alert(data.message);
+    //   // console.log(data.message);
+    //   setMessageReceived(data.message);
+    // });
+    // socket.connect();
+    socket.on("get_location", (data) => {
+      console.log(data);
+    });
+  }, [socket]); // socket이 변경될 때마다(이벤트 발생) useEffect가 실행되도록 종속성 목록에 포함
+
   const [username, setUsername] = useState("");
 
   const [message, setMessage] = useState("");
@@ -78,34 +101,34 @@ const Radar = () => {
       socket.emit("join_room", room); // 해당하는 방 번호에 참여하기 위한 이벤트 발생
     }
   };
+  const sendMessage = () => {
+    socket.emit("send_location", {
+      lng: "127.00004",
+      lat: "67.55553",
+      nickname: "nick",
+      user_id: 6,
+    });
+  };
 
-  setMessage("Hello");
+  // setMessage("Hello");
   // message example
   // socket.emit() : message 방출
   // .emit("전달할 함수 명", 전달할 값)
-  const sendMessage = () => {
-    // socket.emit("send_message", { message: "Hello" });
-    // room을 지정한 경우 이 메시지를 누구에게 보낼지 지정해야 하기 때문에 어느 방에 있는지 보내기
-    socket.emit("send_message", { message, room });
-    // 서버에서는 socket.to(data.room).emit("receive_message", data)로 room에 해당하는 클라이언트에게만 메시지 전송
-  };
-
-  // message를 받을 때마다 호출
-  // 즉, 서버에서 이벤트가 발생할 때마다 실행되는 함수와 같은 역할
-  useEffect(() => {
-    // 이벤트가 발생할 경우를 대비하여 이벤트를 수신하고 있다고 선언
-    // socket.on() : 이벤트 수신
-    // .on("수신 이벤트 함수", 데이터 수신 콜백 함수)
-    socket.on("receive_message", (data) => {
-      // alert(data.message);
-      // console.log(data.message);
-      setMessageReceived(data.message);
-    });
-  }, [socket]); // socket이 변경될 때마다(이벤트 발생) useEffect가 실행되도록 종속성 목록에 포함
+  // const sendMessage = () => {
+  //   // socket.emit("send_message", { message: "Hello" });
+  //   // room을 지정한 경우 이 메시지를 누구에게 보낼지 지정해야 하기 때문에 어느 방에 있는지 보내기
+  //   socket.emit("send_message", { message, room });
+  //   // 서버에서는 socket.to(data.room).emit("receive_message", data)로 room에 해당하는 클라이언트에게만 메시지 전송
+  // };
   return (
-    <OuterCircle>
-      <GreenScanner></GreenScanner>
-    </OuterCircle>
+    <>
+      <button style={{ zIndex: 1000 }} onClick={sendMessage}>
+        위치
+      </button>
+      <OuterCircle>
+        <GreenScanner></GreenScanner>
+      </OuterCircle>
+    </>
   );
 };
 export default Radar;
