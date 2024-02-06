09a2c4bdb9119023e4bd455d13d887242c66fad5
김다희 <dhmonukim24@gmail.com>
2024-02-06 17:56:29 +0900

    feat: battle 소켓 생성 (특정 사용자에게 소켓 전달) S10P12C107-111

--- backend/src/main/java/org/jackpot/back/socket/model/entity/Action.java
+++ backend/src/main/java/org/jackpot/back/socket/model/entity/Action.java
@@ -2,18 +2,20 @@ package org.jackpot.back.socket.model.entity;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.persistence.Entity;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class Location {
-    private String lng;
-    private String lat;
+public class Action {
     private String nickname;
     private Long userId;
     private String socketId;
+    //todo 담을 내용 확정 짓기
+    private Location dataBody;
 }
