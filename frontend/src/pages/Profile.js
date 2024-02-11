7d2a2ef059228ffea2790beeef513d8a69cf40c5
김온유 <gkc04075@gmail.com>
2024-02-12 01:23:29 +0900

    design: 프로필 UI 수정

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 
 const Profile = () => {
 
-
   return (
     <Mobile>
       <Header />
@@ -22,9 +21,12 @@ const Profile = () => {
 export default Profile
 
 const Mobile = styled.div`
-  /* max-width: 400px;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: #f2f2f2; */
-  
+  max-width: 400px;
+  width: 100%;
+  height: 100vh;
+  /* margin-left: auto;
+  margin-right: auto; */
+  background-color: #f2f2f2;
+  display: flex;
+  flex-direction: column;
 `;
