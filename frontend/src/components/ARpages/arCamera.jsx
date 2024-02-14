a3b3c8268dbc17c121c769f47882042a7e1659b3
강승원 <isjj62@naver.com>
2024-02-14 09:06:52 +0900

    fix: camera reloading

--- frontend/src/components/ARpages/arCamera.jsx
+++ frontend/src/components/ARpages/arCamera.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as THREE from "three";
 import html2canvas from "html2canvas";
 import Capture from "./capturePage";
@@ -8,18 +8,21 @@ import Capture from "./capturePage";
 export default function Camera(props) {
   const { state } = useLocation();
   const navigate = useNavigate();
-  const cultural_heritage_id = state.no;
-
-  
-  console.log(state,state.cultural_heritage_id, state.address, "이거는 상속받아온 값입니다.");
+  console.log(state, "state값이예요");
 
   const rendererRef = useRef(null);
   const videoSceneRef = useRef(new THREE.Scene()); // 비디오 씬
   const modelSceneRef = useRef(new THREE.Scene()); // 모델 씬
-  const cameraRef = useRef(new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
+  const cameraRef = useRef(
+    new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+  );
 
   const videoTextureRef = useRef(null);
-  const videoMeshRef = useRef(null);
   const videoStreamRef = useRef(null);
   const canvasRef = useRef(null);
   const [capturedImageDataURL, setCapturedImageDataURL] = useState(null);
@@ -55,11 +58,15 @@ export default function Camera(props) {
   function toggleFacingMode() {
     setFacingMode((prevMode) => (prevMode === "user" ? "environment" : "user"));
   }
-  
 
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
     camera.position.set(0, 0, 10);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     const canvas = document.querySelector("#canvas");
@@ -75,45 +82,50 @@ export default function Camera(props) {
     const ambientLight = new THREE.AmbientLight(0xffffff, 2);
     const directionalLight = new THREE.DirectionalLight(0xff0000, 3);
     directionalLight.position.copy(camera.position);
-    directionalLight.position.set(0, 10, 10); 
-    directionalLight.target.position.set(0,1,1);
+    directionalLight.position.set(0, 10, 10);
+    directionalLight.target.position.set(0, 1, 1);
 
     scene.add(ambientLight);
     scene.add(directionalLight);
-    
+
     modelSceneRef.current.add(ambientLight);
     modelSceneRef.current.add(directionalLight);
 
     const loadGltfModel = (scene) => {
       let loader = new GLTFLoader();
-      loader.load("metarial/1234.gltf", (gltf) => {
-        const model = gltf.scene;
-        model.position.set(-3, -7, -5);
+      loader.load(
+        "metarial/1234.gltf",
+        (gltf) => {
+          const model = gltf.scene;
+          model.position.set(-3, -7, -5);
 
-        // 모델이 로드된 후에 재질에 조명을 추가합니다.
-        model.traverse((child) => {
-          if (child.isMesh) {
-            // 텍스처가 이미 할당되어 있는지 확인
-            if (child.material.map) {
-              // 기존 머티리얼 속성을 유지하면서 새로운 머티리얼 생성
-              child.material = new THREE.MeshStandardMaterial({
-                color: 0xffffff,
-                map: child.material.map,
-                roughness: 0.5,
-                metalness: 0.5,
-                depthTest: true, // 깊이 테스트 사용
-                transparent: false,
-              });
+          // 모델이 로드된 후에 재질에 조명을 추가합니다.
+          model.traverse((child) => {
+            if (child.isMesh) {
+              // 텍스처가 이미 할당되어 있는지 확인
+              if (child.material.map) {
+                // 기존 머티리얼 속성을 유지하면서 새로운 머티리얼 생성
+                child.material = new THREE.MeshStandardMaterial({
+                  color: 0xffffff,
+                  map: child.material.map,
+                  roughness: 0.5,
+                  metalness: 0.5,
+                  depthTest: true, // 깊이 테스트 사용
+                  transparent: false,
+                });
+              }
             }
-          }
-        });
+          });
 
-        // 모델 씬에 모델을 추가합니다.
-        scene.add(model);
-      }, undefined, (error) => {
-        // 모델 로드 실패
-        console.error('An error happened:', error);
-      });
+          // 모델 씬에 모델을 추가합니다.
+          scene.add(model);
+        },
+        undefined,
+        (error) => {
+          // 모델 로드 실패
+          console.error("An error happened:", error);
+        }
+      );
     };
 
     const animate = () => {
@@ -168,6 +180,7 @@ export default function Camera(props) {
           })
           .catch((error) => {
             console.error("Error accessing webcam:", error);
+            constraints.video.facingMode = "environment";
           });
       } catch (error) {
         console.error("Error accessing webcam:", error);
@@ -179,24 +192,18 @@ export default function Camera(props) {
     const resizeCanvas = () => {
       const canvas = rendererRef.current.domElement;
       canvas.width = window.innerWidth * devicePixelRatio;
-      if (window.innerWidth <= 600) {
-        canvas.height = 300 * devicePixelRatio;
-      } else if (window.innerWidth <= 1024) {
-        canvas.height = 400 * devicePixelRatio;
-      } else {
-        canvas.height = 600 * devicePixelRatio;
-      }
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
     };
 
     window.addEventListener("resize", resizeCanvas);
-    resizeCanvas()
+    resizeCanvas();
 
     return () => {
       window.removeEventListener("resize", resizeCanvas);
       stopVideo();
+      startVideo();
     };
   }, [facingMode]);
 
@@ -215,23 +222,15 @@ export default function Camera(props) {
           <Capture
             url={capturedImageDataURL}
             state={state}
-            address= {state.address}
-            cultural_heritage_id = {state.no}
+            address={state.address}
+            cultural_heritage_id={state.no}
             captureState={captureState}
             setCaptureState={setCaptureState}
           />
         )
       ) : (
         <div>
-          <canvas
-            ref={canvasRef}
-            id="canvas"
-            style={{
-              width: "600px",
-              height: "768px",
-              transform: "scaleX(-1)",
-            }}
-          ></canvas>
+          <canvas ref={canvasRef} id="canvas"></canvas>
           <div>
             <button color="primary" onClick={toggleFacingMode}>
               카메라 전환하기
@@ -246,3 +245,4 @@ export default function Camera(props) {
     </div>
   );
 }
+//변경전이예요
