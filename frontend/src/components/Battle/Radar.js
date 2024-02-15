e44dccd6758058610effe3c05ccef7ae3b41b968
thayoon <thagyun@naver.com>
2024-02-16 05:23:07 +0900

    feat: battle Mode change

--- frontend/src/components/Battle/Radar.js
+++ frontend/src/components/Battle/Radar.js
@@ -2,7 +2,24 @@ import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import io from "socket.io-client";
-
+import Battle from "../../assets/Battle.png";
+import useStore from "../../stores/store";
+import { useNavigate } from "react-router-dom";
+const Frame = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background: #dfe7da;
+`;
+const RadarDiv = styled.div`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  bottom: 0px;
+  margin: 0 auto;
+`;
 const OuterCircle = styled.div`
   width: 240px;
   height: 240px;
@@ -56,11 +73,30 @@ const GreenScanner = styled.div`
   z-index: -1;
 `;
 
-const RandomButton = styled.button`
+const RandomButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  // 버튼 커스텀 하기
   position: absolute;
   top: ${(props) => props.$top}%;
   left: ${(props) => props.$left}%;
 `;
+
+const BattleUserName = styled.div`
+  font-family: "omyu_pretty";
+  font-size: large;
+`;
+
+const BattleUserIMG = styled.div`
+  background-image: url(${Battle});
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 5rem;
+  height: 5rem;
+`;
+
 const Radar = () => {
   const user = JSON.parse(localStorage.getItem("user")); // user 정보
   const [myGeo, setMyGeo] = useState({}); // 현재 사용자 위치 정보
@@ -68,7 +104,10 @@ const Radar = () => {
   const mySocket = useRef(null); // 현재 사용자 소켓 정보
   const [userList, setUserList] = useState([]); // 현재 사용자 주변 사용자 리스트
   const [battleState, setBattleState] = useState(false); // 현재 배틀 상태 정보
-
+  const [holdingList, setHoldingList] = useState([]); // 사용자 보유 카드 리스트
+  const [selectCard, setSelectCard] = useState(null); // 선택 카드 번호
+  const [nextState, setNextState] = useState(false);
+  let navigate = useNavigate();
   // 소켓 연결
   useEffect(() => {
     // socket connection
@@ -78,6 +117,8 @@ const Radar = () => {
         transports: ["websocket"],
       })
     );
+    // 보유 카드 리스트 정보
+    getHoldingList();
   }, []); // 처음 렌더링 될 때만 실행
 
   // 현재 사용자 위치 정보
@@ -121,7 +162,6 @@ const Radar = () => {
       // 다른 사용자 정보 리스닝 이벤트
       socket.on("get_location", (data) => {
         if (mySocket.current === null && data.user_id === user.id) {
-          // setMySocket(data);
           mySocket.current = data;
         }
         if (data.user_id !== user.id) {
@@ -139,24 +179,34 @@ const Radar = () => {
       socket.on("get_message", (data) => {
         console.log(data);
         // let msg = window.confirm(data.message);
-        let msg = getConfirm(data.message);
-        console.log(msg);
-        if (msg) {
+        if (data.message.includes("배틀을 신청하였습니다.")) {
+          let msg = getConfirm(data.message);
+          console.log(msg);
+          if (msg) {
+            // 배틀 신청 수락 emit
+            socket.emit("send_message", {
+              socket_id: data.opponent_socket_id,
+              opponent_socket_id: data.socket_id,
+              message: `${user.nickname}님이 배틀을 수락하였습니다.`,
+            });
+            alert("배틀을 시작합니다. 제출 카드를 선택해주세요.");
+            setBattleState(true);
+          } else {
+            // 배틀 신청 거절 emit
+            socket.emit("send_message", {
+              socket_id: data.opponent_socket_id,
+              opponent_socket_id: data.socket_id,
+              message: `${user.nickname}님이 배틀을 거절하였습니다.`,
+            });
+            alert("배틀이 거절됐습니다.");
+            setBattleState(false);
+          }
+        } else if (data.message.includes("배틀을 수락하였습니다.")) {
+          alert("배틀을 시작합니다. 제출 카드를 선택해주세요.");
           setBattleState(true);
-          // 배틀 신청 수락 emit
-          socket.emit("send_message", {
-            socket_id: data.opponent_socket_id,
-            opponent_socket_id: data.socket_id,
-            message: `${user.nickname}님이 배틀을 수락하였습니다.`,
-          });
-        } else {
+        } else if (data.message.includes("배틀을 거절하였습니다.")) {
+          alert("배틀이 거절됐습니다.");
           setBattleState(false);
-          // 배틀 신청 거절 emit
-          socket.emit("send_message", {
-            socket_id: mySocket.current?.socket_id,
-            opponent_socket_id: data.opponent_socket_id,
-            message: `${user.nickname}님이 배틀을 거절하였습니다.`,
-          });
         }
       });
 
@@ -218,29 +268,92 @@ const Radar = () => {
     });
     console.log(res);
   };
-
+  // 사용자 보유 카드 리스트
+  const getHoldingList = async () => {
+    try {
+      const res = await axios.get("/v1/card/holding_list");
+      console.log(res);
+      if (res.data.data_body.holding_cards.length < 3) {
+        alert("문화재 카드를 3장 이상 수집해주세요.");
+        navigate("/maps");
+      } else {
+        setHoldingList(res.data.data_body.holding_cards);
+      }
+    } catch (error) {
+      console.log(error.response);
+      if (error.response.status === 500) {
+        await useStore.getState().updateToken();
+        getHoldingList();
+      }
+    }
+  };
+  const handleNext = () => {
+    if (selectCard === "" || selectCard === null || selectCard === undefined) {
+      alert("제출 카드를 선택해주세요.");
+    } else {
+      setNextState(true);
+    }
+  };
   return (
     <>
-      {userList.map((user) => (
-        <RandomButton
-          key={user.user_id}
-          $top={Math.random() * 100} // top 위치를 랜덤하게 결정합니다.
-          $left={Math.random() * 100} // left 위치를 랜덤하게 결정합니다.
-          onClick={() => sendMessageToUser(user)}
-        >
-          {user.nickname}
-        </RandomButton>
-      ))}
+      {battleState && !nextState && (
+        <Frame>
+          <table>
+            <thead>
+              <tr>
+                <th>선택</th>
+                <th>문화재명</th>
+                <th>속성</th>
+                <th>등급</th>
+                <th>보유개수</th>
+              </tr>
+            </thead>
+            <tbody>
+              {holdingList.map((data) => (
+                <tr key={data.card_number}>
+                  <td>
+                    <input
+                      onClick={() => setSelectCard(data.card_number)}
+                      type="radio"
+                      checked={selectCard === data.card_number}
+                    />
+                  </td>
+                  <td>{data.cultural_heritage_name}</td>
+                  <td>{data.field}</td>
+                  <td>{data.grade}</td>
+                  <td>{data.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button onClick={handleNext}>완료</button>
+        </Frame>
+      )}
+      {!battleState && !nextState && (
+        <RadarDiv>
+          {userList.map((user) => (
+            <RandomButton
+              key={user.user_id}
+              $top={Math.random() * 100} // top 위치를 랜덤하게 결정합니다.
+              $left={Math.random() * 100} // left 위치를 랜덤하게 결정합니다.
+              onClick={() => sendMessageToUser(user)}
+            >
+              <BattleUserIMG></BattleUserIMG>
+              <BattleUserName>{user.nickname}</BattleUserName>
+            </RandomButton>
+          ))}
 
-      {/* <button style={{ zIndex: 1000 }} onClick={sendMessage}>
+          {/* <button style={{ zIndex: 1000 }} onClick={sendMessage}>
         위치
       </button> */}
-      <button style={{ zIndex: 1000 }} onClick={sendQuestion}>
-        문제 제시
-      </button>
-      <OuterCircle>
-        <GreenScanner></GreenScanner>
-      </OuterCircle>
+          {/* <button style={{ zIndex: 1000 }} onClick={sendQuestion}>
+            문제 제시
+          </button> */}
+          <OuterCircle>
+            <GreenScanner></GreenScanner>
+          </OuterCircle>
+        </RadarDiv>
+      )}
     </>
   );
 };
