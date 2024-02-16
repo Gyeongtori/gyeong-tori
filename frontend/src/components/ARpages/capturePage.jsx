a2a8bbc91e94edb3d4ae3a1311a00d1978b15215
강승원 <isjj62@naver.com>
2024-02-16 09:17:18 +0900

    fix: capturePage.hotfix

--- frontend/src/components/ARpages/capturePage.jsx
+++ frontend/src/components/ARpages/capturePage.jsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import Maps from "../../pages/Maps";
-import html2canvas from "html2canvas";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -8,24 +6,26 @@ export default function Capture(props) {
   console.log(props.url, props.state);
   const url = props.url;
   const state = props.state;
-  const cultural_heritage_id = parseInt(state.no);
+  const cultural_heritage_id = props.cultural_heritage_id;
   const address = state.address;
 
   const navigate = useNavigate();
-  const backMap = () => {
-    navigate("/maps");
+  const backMap = async () => {
+    try {
+      await navigate("/maps");
+      window.location.reload();
+    } catch (error) {
+      console.error("백으로 돌아갈 수 없어", error);
+    }
   };
-  const [api, setApi] = useState();
+
   const postAPI = async () => {
     try {
       const postData = {
         cultural_heritage_id: cultural_heritage_id,
         address: address,
       };
-      const res = await axios.post(`/v1/card/add`, {
-        cultural_heritage_id: cultural_heritage_id,
-        address: address,
-      });
+      const res = await axios.post(`/v1/card/add`, postData);
       console.log(res);
       console.log("정상적으로 실행되었습니다.");
     } catch (e) {
@@ -33,7 +33,7 @@ export default function Capture(props) {
     }
   };
 
-  const handleDownload = () => {
+  const handleDownload = async () => {
     // 캡쳐된 이미지 데이터 URL을 이용하여 다운로드
     if (url) {
       const downloadLink = document.createElement("a");
@@ -44,24 +44,38 @@ export default function Capture(props) {
       document.body.appendChild(downloadLink);
       downloadLink.click();
       document.body.removeChild(downloadLink);
-      alert("사진이 저장되었습니다.");
-      postAPI();
+      await postAPI();
       backMap();
+      alert("사진이 저장되었습니다.");
     } else {
       console.error("No captured image data to download.");
     }
   };
-  const handleCloseModal = () => {
+  const handleCloseModal = async () => {
     // 모달 상태를 닫음
     props.setCaptureState(false);
-    postAPI();
+    await postAPI();
     backMap();
   };
 
   return (
-    <div>
+    <div id="capture">
       {props.captureState && (
         <div>
+      
+
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div style={{ width: "100%", height: "90%" }}>
+              {
+                <img
+                  src={url}
+                  alt="Captured"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              }
+            </div>
+          </div>
+
           <div style={{ display: "flex", justifyContent: "center" }}>
             <button
               style={{ marginRight: "10px", marginBottom: "10px" }}
@@ -74,17 +88,6 @@ export default function Capture(props) {
             </button>
           </div>
 
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <div style={{ width: "300px", height: "642px" }}>
-              {
-                <img
-                  src={url}
-                  alt="Captured"
-                  style={{ width: "100%", height: "100%" }}
-                />
-              }
-            </div>
-          </div>
         </div>
       )}
       {/* 모달 상태가 true일 때만 모달을 렌더링 */}
